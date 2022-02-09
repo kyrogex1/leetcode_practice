7d72b5939e2be7b34ecc0fefc51f0cb742c8cf05
JOSHHH <josh0039@e.ntu.edu.sg>
2022-02-09 16:03:40 +0800

    Completed 901

---
+++ 901.py
@@ -0,0 +1,40 @@
+from re import I
+from typing import *
+
+# 901. Online Stock Span
+# https://leetcode.com/problems/online-stock-span/
+
+# Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+# The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
+
+# For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85], then the stock spans would be [1,1,1,2,1,4,6].
+# Implement the StockSpanner class:
+
+# StockSpanner() Initializes the object of the class.
+# int next(int price) Returns the span of the stock's price given that today's price is price.
+
+class StockSpanner:
+
+    def __init__(self):
+        self.prices = []
+        self.spans = []
+        
+    # Note my solution uses unecessary space.
+    # There are more optimal solutions using monotonic stack in discussion
+    def next(self, price: int) -> int:
+        self.prices.append(price)
+        self.spans.append(1)
+        
+        counter = 0
+        idx = len(self.spans) - 1
+        while(idx > -1 and price >= self.prices[idx]):
+            counter += self.spans[idx]
+            idx -= self.spans[idx]
+            
+        self.spans[-1] = counter
+        
+        return counter
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
